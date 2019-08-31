3a8e12692c042b9b9a52bceb9db2bc63f28b2e96
Do Minh Thang <dominhthang1999@gmail.com>
2019-08-31 14:07:34 +0700

    Done

--- 04-Kmeans_Clustering/main.py
+++ 04-Kmeans_Clustering/main.py
@@ -24,10 +24,59 @@ def kmeans_display(X, label):
 
     plt.plot( X0[:, 0], X0[:, 1], 'b^', markersize = 4, alpha =.8)
     plt.plot( X1[:, 0], X1[:, 1], 'go', markersize = 4, alpha =.8)
-    plt.plot( X2[:, 0], X2[:, 1], 'rs', markersize =4, alpha = .8)
+    plt.plot( X2[:, 0], X2[:, 1], 'rs', markersize = 4, alpha =.8)
 
     plt.axis('equal')
     plt.plot()
     plt.show()
 
 kmeans_display(X, original_label)
+
+# Khởi tạo các centers ban đầu
+def kmeans_innit_centers(X, K):
+    # randomly pick k rows of X as initial centers
+    return X[np.random.choice( X.shape[0], K, replace = False)]
+
+# Gán nhãn mới cho các điểm khi biết các centers
+def kmeans_assign_labels(X, centers):
+    # calculate pairwise distances btw data and centers
+    D = cdist(X, centers)
+    # return index of the closest center
+    return np.argmin(D, axis = 1)
+
+# Cập nhập vị trí các điểm center, dựa trên data vừa được gắn label
+def kmaens_update_centers(X, labels, K):
+    centers = np.zeros((K, X.shape[1]))
+    for k in range(K):
+        # collect all points assigned to the k-th cluster
+        Xk = X[labels == k, :]
+        # take average
+        centers[k, :] = np.mean(Xk, axis = 0)
+    return centers
+
+# Kiểm tra điều kiện dừng của thuật toán, nếu
+def has_converged(centers, new_centers):
+    # return True if two sets of centers are the same
+    return (set([tuple(a) for a in centers]) ==
+            set([tuple(a) for a in new_centers]))
+
+def kmeans(X, K):
+    centers = [kmeans_innit_centers(X, K)]
+    labels = []
+    it = 0
+    while True:
+        labels.append(kmeans_assign_labels(X, centers[-1]))
+        new_centers = kmaens_update_centers(X, labels[-1], K)
+        if has_converged(centers[-1], new_centers):
+            break
+        centers.append(new_centers)
+        it += 1
+    return (centers, labels, it)
+
+(centers, labels, it) = kmeans(X, K)
+print('Centers found by our algorithm:')
+print(centers[-1])
+
+kmeans_display(X, labels[-1])
+
+
